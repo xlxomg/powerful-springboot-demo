6e56d0346b8d233c09f53a8d7022adefe177e79e
xieluxin <xieluxin@qq.com>
2020-03-17 22:24:53 +0800

    细节调整

--- src/main/java/com/xlx/powerfuldemo/common/constant/ProjectConstant.java
+++ src/main/java/com/xlx/powerfuldemo/common/constant/ProjectConstant.java
@@ -8,4 +8,14 @@ package com.xlx.powerfuldemo.common.constant;
  */
 public final  class ProjectConstant {
     public static final String SESSION_USER = "userAccount";
+
+    /**
+     * 常用符号
+     */
+    public static final String SEMICOLON_SYMBOL = ":";
+    public static final String COMMA_SYMBOL = ",";
+    /**
+     * 唯一分隔符号
+     */
+    public static final String UNIQUE_SPLIT_SYMBOL = "@@##@@";
 }
